0243bed85e33b9e0f5b58b53533a4f4ca8fdb8d1
Reuel Sicat II <reuel@truelogic.com.ph>
2020-01-12 01:27:17 +0800

    CodeIgniter Tutorial
    URL: --------------------------
    
    Title
    * TaskForm - Part 04 of XX
    
    Notes:
    * Adding Session to connect Login to Task
    * Bug: Auto add task entry if page is refresh

--- application/controllers/Task.php
+++ application/controllers/Task.php
@@ -8,10 +8,9 @@ class Task extends CI_Controller
     {
         echo "Class: Login -> Function: index";
         echo "<br>";
-        echo "Difference between two dates(Hr): " .(strtotime("2020-01-11 01:43:10") - strtotime("2020-01-10 15:43:08"))/60/60;
+        echo "Difference between two dates(Hr): " . (strtotime("2020-01-11 01:43:10") - strtotime("2020-01-10 15:43:08")) / 60 / 60;
         echo "<br>";
-        echo "Difference between two dates(Hrs): " .gmdate("H:i:s", (strtotime("2020-01-11 01:43:10") - strtotime("2020-01-10 15:43:08")));
-
+        echo "Difference between two dates(Hrs): " . gmdate("H:i:s", (strtotime("2020-01-11 01:43:10") - strtotime("2020-01-10 15:43:08")));
     }
 
     public function __construct()
@@ -33,11 +32,11 @@ class Task extends CI_Controller
 
     function create()
     {
+        $seesdata = $this->session->all_userdata();
+        if (isset($this->session->userdata['logged_in']) && $this->input->post('type') && $this->input->post('department')) {
 
-        if (isset($this->session->userdata['logged_in']))
-        {
+            echo "=========aaaaaaaaaaaaa==================";
             
-            $seesdata = $this->session->all_userdata();
             $data = array(
                 'reg_id' => $seesdata['logged_in']['regid'],
                 'type' => $this->input->post('type'),
@@ -47,26 +46,35 @@ class Task extends CI_Controller
                 'elapsed' => 0,
                 'remarks' => $this->input->post('remarks')
             );
-            echo "=========withsessiontop==================";
+            // echo "=========withsessiontop==================";
             $result['createtaskresult'] = $this->task_model->create_task($data);
-            $result['tasks'] = $this->task_model->get_allbyRegID($data);
+            $result['tasks'] = $this->task_model->get_allbyRegID($seesdata['logged_in']['regid']);
             $this->load->view('TaskForm', $result);
-        } else {
             
-            echo "=========withoutsession==================";
+        } elseif (isset($this->session->userdata['logged_in']) && ! $this->input->post('username') && ! $this->input->post('password')) {
+            
+            echo "=========sdfsfadasd==================";
+            $result['tasks'] = $this->task_model->get_allbyRegID($seesdata['logged_in']['regid']);
+            $this->load->view('TaskForm', $result);
+        } 
+        else {
+
+            // echo "=========withoutsession==================";
+            $data = array(
+                'error_message' => 'Your Session has expired. Please login.'
+            );
             $this->load->view('LoginForm');
         }
     }
-    
+
     function update()
     {
-        
         $this->form_validation->set_rules('type', 'Task Type', 'trim|required');
         $this->form_validation->set_rules('department', 'Department', 'trim|required');
-        
+
         if ($this->form_validation->run() === FALSE) {
             if (isset($this->session->userdata['logged_in'])) {
-                
+
                 $seesdata = $this->session->all_userdata();
                 $data = array(
                     'reg_id' => $seesdata['logged_in']['regid'],
@@ -77,11 +85,11 @@ class Task extends CI_Controller
                     'elapsed' => 0,
                     'remarks' => $this->input->post('remarks')
                 );
-                
+
                 $result['resultBool'] = $this->task_model->create_task($data);
                 $this->load->view('TaskForm', $result);
             } else {
-                
+
                 $this->load->view('TaskForm');
             }
         } else {
@@ -95,7 +103,7 @@ class Task extends CI_Controller
                 'elapsed' => 0,
                 'remarks' => $this->input->post('remarks')
             );
-            
+
             $result['resultMessage'] = $this->task_model->create_task($data);
             $this->load->view('TaskForm', $result);
         }
